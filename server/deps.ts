5aed89c2cb96080ce91a558f0423da32bf24b53a
Marcelo Pecin <celopeccin@gmail.com>
2021-11-11 23:32:53 -0300

    fix: dependencies no more used

--- server/deps.ts
+++ server/deps.ts
@@ -1,5 +1,3 @@
-export { listenAndServe } from 'https://deno.land/std/http/server.ts'
-export { acceptWebSocket, acceptable } from 'https://deno.land/std/ws/mod.ts'
 export { config as env } from 'https://deno.land/x/dotenv/mod.ts'
 export {
   Database as MySQLDatabase,
